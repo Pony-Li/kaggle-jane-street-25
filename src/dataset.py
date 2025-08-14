70f187f80e4a435ad729d63d5d9fab4efb207ea3
Pony-Li <2100011004@stu.pku.edu.cn>
2025-08-14 12:20:59 +0800

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -16,26 +16,26 @@ class TimeSeriesDataset(Dataset):
 
     def _prepare_data(self, data, feature_columns, target_column, weight_column):
         features, targets, weights, masks = [], [], [], []
-        grouped_by_date = data.groupby("date_id")  # 对全量的训练数据先按照日期分组 (date, symble_id, time, feature)
+        grouped_by_date = data.groupby("date_id")  # 对全量的数据按照日期分组
 
-        # 按照日期循环迭代数据, 每个 date_group 是三维张量, 形状为 (time_steps=968, num_symbols=39, num_features=79)
+        # 按照日期循环迭代数据, 每个 date_group 是 (time_steps=968)×(num_symbols=39) 行的 dataframe
         for date_id, date_group in tqdm(grouped_by_date, desc="Processing Dates", total=len(grouped_by_date)):
 
-            # 初始化四个常值张量存储单日数据, 包括特征数据、目标值数据、 权重数据 (用于计算 r2) 和掩码数据 (用来标记哪些时间步是真实数据, 哪些是填充数据)
-            date_features = torch.zeros((self.num_symbols, self.time_steps, len(feature_columns)))  # 初始化特征张量（39, 968, 79)
+            # 初始化四个常值张量存储单日数据, 包括特征数据、目标值数据、权重数据 (用于计算 r2) 和掩码数据 (用来标记哪些时间步是真实数据, 哪些是填充数据)
+            date_features = torch.zeros((self.num_symbols, self.time_steps, len(feature_columns)))  # 初始化特征张量 (39, 968, num_features)
             date_targets = torch.zeros((self.num_symbols, self.time_steps))  # 初始化目标张量 (39, 968)
             date_weights = torch.zeros((self.num_symbols, self.time_steps))  # 初始化权重张量(39, 968)
             date_masks = torch.ones((self.num_symbols, self.time_steps))  # 初始化掩码张量(39, 968)
 
-            grouped_by_symbol = date_group.groupby("symbol_id")  # 按符号分组
+            grouped_by_symbol = date_group.groupby("symbol_id")  # 将单日的数据按 symbol_id 分组
 
-            # 按照 symbol_id 迭代循环每天的数据, 每个 group 是一个二维张量, 形状为 (time_steps=968, num_features=79)
+            # 按照 symbol_id 迭代循环每天的数据, 每个 group 是 (time_steps=968) 行的 dataframe
             for symbol_id, group in grouped_by_symbol:
 
                 group = group.sort_values("time_id")  # 按时间排序
-                x = group[feature_columns].values  # 特征值
-                y = group[target_column].values  # 目标值
-                w = group[weight_column].values  # 权重值
+                x = group[feature_columns].values  # 特征值, (time_steps=968) 行 num_features 列的 dataframe
+                y = group[target_column].values  # 目标值, (time_steps=968) 行 1 列的 dataframe
+                w = group[weight_column].values  # 权重值, (time_steps=968) 行 1 列的 dataframe
 
                 # 如果某个 symbol_id 对应数据的 time_steps 数量 > 968, 那么就只保留最后 968 个 time_steps 的数据
                 if len(group) > self.time_steps:
@@ -49,20 +49,19 @@ class TimeSeriesDataset(Dataset):
                     w = np.pad(w, (0, pad_size), mode="constant")  # 用 0 填充权重
                     date_masks[symbol_id, -pad_size:] = 0  # 更新掩码, 将 padding 数据对应部分的掩码设置为 0
 
-                # x.shape: (968, 79), y.shape: (968,), w.shape: (968,)
-
-                date_features[symbol_id] = torch.FloatTensor(x)  # 将修剪工整的单个 symbol_id 的特征数据 (79 个 features) 转换为 torch 张量后存入提前创建好的单日特征数据张量
+                # x.shape: (968, num_features), y.shape: (968,), w.shape: (968,)
+                date_features[symbol_id] = torch.FloatTensor(x)  # 将修剪工整的单个 symbol_id 的特征数据转换为 torch 张量后存入提前创建好的单日特征数据张量
                 date_targets[symbol_id] = torch.FloatTensor(y)  # 将修剪工整的单个 symbol_id 的目标值数据 (responsder_6) 转换为 torch 张量后存入提前创建好的单日目标值数据张量
                 date_weights[symbol_id] = torch.FloatTensor(w)  # 将修剪工整的单个 symbol_id 的权重数据转换为 torch 张量后存入提前创建好的单日权重数据张量
 
             # 将收集好的单日数据按次序 append 到空列表中
-            features.append(date_features)  # 添加特征, 循环结束时的结构: num_dates=560 个形状为 (num_symbols=39, time_steps=968, num_features=79) 的三维 torch 张量组成的列表
+            features.append(date_features)  # 添加特征, 循环结束时的结构: num_dates=560 个形状为 (num_symbols=39, time_steps=968, num_features) 的三维 torch 张量组成的列表
             targets.append(date_targets)  # 添加目标, 循环结束时的结构: num_dates=560 个形状为 (num_symbols=39, time_steps=968) 的二维 torch 张量组成的列表
             weights.append(date_weights)  # 添加权重, 循环结束时的结构: num_dates=560 个形状为 (num_symbols=39, time_steps=968) 的二维 torch 张量组成的列表
             masks.append(date_masks)  # 添加掩码, 循环结束时的结构: num_dates=560 个形状为 (num_symbols=39, time_steps=968) 的二维 torch 张量组成的列表
 
-        # 将由多个 torch 张量组成的列表堆叠成一个新的高维张量
-        features = torch.stack(features).to(self.device)  # 堆叠特征并移动到设备, shape: (num_dates=560, num_symbols=39, time_steps=968, num_features=79)
+        # 将由多个 torch 张量组成的列表堆叠成一个新的高维 torch 张量
+        features = torch.stack(features).to(self.device)  # 堆叠特征并移动到设备, shape: (num_dates=560, num_symbols=39, time_steps=968, num_features)
         targets = torch.stack(targets).to(self.device)  # 堆叠目标并移动到设备, shape: (num_dates=560, num_symbols=39, time_steps=968)
         weights = torch.stack(weights).to(self.device)  # 堆叠权重并移动到设备, shape: (num_dates=560, num_symbols=39, time_steps=968)
         masks = torch.stack(masks).to(self.device)  # 堆叠掩码并移动到设备, shape: (num_dates=560, num_symbols=39, time_steps=968)
@@ -82,7 +81,7 @@ class TimeSeriesDataModule(LightningDataModule):
         self.train_data = train_data  # 训练数据
         self.valid_data = valid_data  # 验证数据
         self.feature_columns = feature_columns  # 特征列
-        self.batch_size = batch_size  # 批量大小
+        self.batch_size = batch_size  # 批量大小, 我将 batch_size 设置为 1, 即每次迭代得到的都是单日数据
         self.device = device  # 设备
         self.num_symbols = num_symbols  # 符号数量
         self.time_steps = time_steps  # 时间步长
